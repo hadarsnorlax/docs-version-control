9fedb88e70cd8b74b8e24231588aabf7a7296250
hadarsnorlax <hadar.mirsky1@gmail.com>
2023-04-20 13:53:32 +0300

    Improve writing page layout

--- src/App.js
+++ src/App.js
@@ -2,31 +2,20 @@ import React, { useState } from 'react';
 import Editor from './components/Editor/Editor';
 import FileTree from './components/FileTree/FileTree';
 import fileTreeData from './components/FileTree/data';
+import './App.css';
 
 const App = () => {
-  const [content, setContent] = useState('');
-  const [savedContent, setSavedContent] = useState('');
+  const [fileContent, setFileContent] = useState('');
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Docs Version Control</h1>
-      </header>
-      <main>
-        <FileTree
-          data={fileTreeData}
-          onSelect={(node) => console.log('Selected:', node)}
-        />
-        <Editor
-          fileContent={content}
-          onContentChange={setContent}
-          onSave={(newContent) => setSavedContent(newContent)}
-        />
-        <div>
-          <h2>Saved Content:</h2>
-          <pre>{savedContent}</pre>
-        </div>
-      </main>
+    <div className="app-container">
+      <FileTree
+        data={fileTreeData}
+        onSelect={(node) => console.log('Selected:', node)}
+      />
+      <div className="editor-container">
+        <Editor fileContent={fileContent} onContentChange={setFileContent} />
+      </div>
     </div>
   );
 };
